0544cddc9b283b4d0878d144cb672bdc04fbfbcd
joaodiassenac <joaopedroadias61@gmail.com>
2026-07-31 14:29:18 -0300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import { errorHandler } from './middlewares/errorHandler';
 import { notFoundHandler } from './middlewares/notFoundHandler';
+const cors = require('cors');
 import routes from './routes';
 
+
 const app = express();
+const cors = require('cors');
+app.use(cors({
+  origin: 'https://seu-front.vercel.app'
+}));
 
 app.use(cors());
 app.use(express.json());
