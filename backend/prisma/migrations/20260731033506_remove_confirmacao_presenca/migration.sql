78f20def52abe7e26343e64f09610db462db98e9
João Pedro de Araújo Dias <joao61732576@edu.df.senac.br>
2026-07-31 15:36:06 -0300

    alteracao migration

--- backend/prisma/migrations/20260731033506_remove_confirmacao_presenca/migration.sql
+++ backend/prisma/migrations/20260731033506_remove_confirmacao_presenca/migration.sql
@@ -1,11 +1,11 @@
 /*
   Warnings:
 
-  - You are about to drop the `confirmacaopresenca` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `ConfirmacaoPresenca` table. If the table is not empty, all the data it contains will be lost.
 
 */
 -- DropForeignKey
-ALTER TABLE `confirmacaopresenca` DROP FOREIGN KEY `ConfirmacaoPresenca_eventoId_fkey`;
+ALTER TABLE `ConfirmacaoPresenca` DROP FOREIGN KEY `ConfirmacaoPresenca_eventoId_fkey`;
 
 -- DropTable
-DROP TABLE `confirmacaopresenca`;
+DROP TABLE `ConfirmacaoPresenca`;
