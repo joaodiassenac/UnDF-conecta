78b8b07728745a40ddd28eb141c311a0cf063b13
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2026-07-31 00:50:27 -0300

    routes criados

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import { errorHandler } from './middlewares/errorHandler';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Rotas serão registradas aqui conforme forem criadas
-// app.use('/api/eventos', eventosRoutes);
+app.use('/api', routes);
 
 app.use(errorHandler);
 
